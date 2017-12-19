0be047a473951badbcbc2890b6ac73ab721950ee
1790618804 <1790618804@.qqcom>
2017-12-19 11:27:43 +0800

    第一个版本

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/12/13 0013.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,22 @@
+package com.coolweather.android.gson;
+
+import android.text.style.UpdateAppearance;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/12/13 0013.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
